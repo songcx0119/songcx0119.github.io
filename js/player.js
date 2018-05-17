bbed2770a4c704157b00420296ea79bb3a546dc8
song_c <song_c@worksap.co.jp>
2018-05-17 19:12:17 +0800

    fix volume bar and some adjustment

--- js/player.js
+++ js/player.js
@@ -6,29 +6,11 @@ var progress = audioPlayer.querySelector('.progress');
 var sliders = audioPlayer.querySelectorAll('.slider');
 var volumeBtn = audioPlayer.querySelector('.volume-btn');
 var volumeControls = audioPlayer.querySelector('.volume-controls');
-var volumeProgress = volumeControls.querySelector('.slider .progress');
+var volumeProgress = volumeControls.querySelector('.slider #myRange');
 var player = audioPlayer.querySelector('audio');
 var currentTime = audioPlayer.querySelector('.current-time');
 var totalTime = audioPlayer.querySelector('.total-time');
 var speaker = audioPlayer.querySelector('#speaker');
-
-var draggableClasses = ['pin'];
-var currentlyDragged = null;
-
-window.addEventListener('mousedown', function (event) {
-
-	if (!isDraggable(event.target)) return false;
-
-	currentlyDragged = event.target;
-	var handleMethod = currentlyDragged.dataset.method;
-
-	this.addEventListener('mousemove', window[handleMethod], false);
-
-	window.addEventListener('mouseup', function () {
-		currentlyDragged = false;
-		window.removeEventListener('mousemove', window[handleMethod], false);
-	}, false);
-});
 
 playBtn.addEventListener('click', togglePlay);
 pauseBtn.addEventListener('click', togglePlay);
@@ -49,38 +31,13 @@ volumeBtn.addEventListener('click', function () {
 	volumeControls.classList.toggle('hidden');
 });
 
-window.addEventListener('resize', directionAware);
+//window.addEventListener('resize', directionAware);
+//
+//directionAware();
 
-sliders.forEach(function (slider) {
-	var pin = slider.querySelector('.pin');
-	slider.addEventListener('click', window[pin.dataset.method]);
-});
-
-directionAware();
-
-function isDraggable(el) {
-	var canDrag = false;
-	var classes = Array.from(el.classList);
-	draggableClasses.forEach(function (draggable) {
-		if (classes.indexOf(draggable) !== -1) canDrag = true;
-	});
-	return canDrag;
-}
-
-function inRange(event) {
-	var rangeBox = getRangeBox(event);
-	var rect = rangeBox.getBoundingClientRect();
-	var direction = rangeBox.dataset.direction;
-	if (direction == 'horizontal') {
-		var min = rangeBox.offsetLeft;
-		var max = min + rangeBox.offsetWidth;
-		if (event.clientX < min || event.clientX > max) return false;
-	} else {
-		var min = rect.top;
-		var max = min + rangeBox.offsetHeight;
-		if (event.clientY < min || event.clientY > max) return false;
-	}
-	return true;
+volumeProgress.oninput = function () {
+	changeVolume(this.value);
+	updateVolume(this.value);
 }
 
 function updateProgress() {
@@ -91,45 +48,14 @@ function updateProgress() {
 	currentTime.textContent = formatTime(current);
 }
 
-function updateVolume() {
-	progress.style.width = player.volume * 100 + '%';
-	if (player.volume >= 0.5) {
+function updateVolume(value) {
+	if (value >= 50) {
 		speaker.attributes.d.value = 'M14.667 0v2.747c3.853 1.146 6.666 4.72 6.666 8.946 0 4.227-2.813 7.787-6.666 8.934v2.76C20 22.173 24 17.4 24 11.693 24 5.987 20 1.213 14.667 0zM18 11.693c0-2.36-1.333-4.386-3.333-5.373v10.707c2-.947 3.333-2.987 3.333-5.334zm-18-4v8h5.333L12 22.36V1.027L5.333 7.693H0z';
-	} else if (player.volume < 0.5 && player.volume > 0.05) {
+	} else if (value < 50 && value > 5) {
 		speaker.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667M17.333 11.373C17.333 9.013 16 6.987 14 6v10.707c2-.947 3.333-2.987 3.333-5.334z';
-	} else if (player.volume <= 0.05) {
+	} else if (value <= 5) {
 		speaker.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667';
 	}
-}
-
-function getRangeBox(event) {
-	var rangeBox = event.target;
-	var el = currentlyDragged;
-	if (event.type == 'click' && isDraggable(event.target)) {
-		rangeBox = event.target.parentElement.parentElement;
-	}
-	if (event.type == 'mousemove') {
-		rangeBox = el.parentElement.parentElement;
-	}
-	return rangeBox;
-}
-
-function getCoefficient(event) {
-	var slider = getRangeBox(event);
-	var rect = slider.getBoundingClientRect();
-	var K = 0;
-	if (slider.dataset.direction == 'horizontal') {
-
-		var offsetX = event.clientX - slider.offsetLeft;
-		var width = slider.clientWidth;
-		K = offsetX / width;
-	} else if (slider.dataset.direction == 'vertical') {
-
-		var height = slider.clientHeight;
-		var offsetY = event.clientY - rect.top;
-		K = 1 - offsetY / height;
-	}
-	return K;
 }
 
 function rewind(event) {
@@ -138,9 +64,9 @@ function rewind(event) {
 	}
 }
 
-function changeVolume(event) {
-	if (inRange(event)) {
-		player.volume = getCoefficient(event);
+function changeVolume(value) {
+	if (value <= 100 && value > 0) {
+		player.volume = value / 100;
 	}
 }
 
@@ -168,15 +94,15 @@ function makePlay() {
 	loading.style.display = 'none';
 }
 
-function directionAware() {
-	if (window.innerHeight < 250) {
-		volumeControls.style.bottom = '-54px';
-		volumeControls.style.left = '54px';
-	} else if (audioPlayer.offsetTop < 154) {
-		volumeControls.style.bottom = '-164px';
-		volumeControls.style.left = '-3px';
-	} else {
-		volumeControls.style.bottom = '52px';
-		volumeControls.style.left = '-3px';
-	}
-}
+//function directionAware() {
+//	if (window.innerHeight < 250) {
+//		volumeControls.style.bottom = '0px';
+//		volumeControls.style.left = '40px';
+//	} else if (audioPlayer.offsetTop < 154) {
+//		volumeControls.style.bottom = '0px';
+//		volumeControls.style.left = '40px';
+//	} else {
+//		volumeControls.style.bottom = '0px';
+//		volumeControls.style.left = '40px';
+//	}
+//}

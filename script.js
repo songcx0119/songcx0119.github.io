1d1786d8e2119d9f055e0dc5c0efdcf2ddb9d54a
songcx0119 <songcx1994@gmail.com>
2021-11-23 19:59:01 +0800

    saki happy birthday

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "Eman Sabir" && b=="27-06-1996")
-  	{
-  	    window.location.href="start.html";
-  	}
-  	else
-  	{
-  		alert("invalid details");
-  		window.location.href="index.html";
-  	}
+var name = prompt("请问你的名字:");
+if(name == "saki")
+{
+  window.location.href="start.html";
+}
+else
+{
+  alert("只给saki看哦..");
+  window.location.href="index.html";
+}
   	
